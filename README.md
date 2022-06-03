087fbfaf7e0df16dec57aa421520d47d34a24c3c
Thomas Mock <j.thomasmock@gmail.com>
2022-06-03 09:04:58 -0500

    update slides, add image to readme, bump font size for code chunks

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 # arrow-dplyr
 
 This is a brief, 10 min lightning talk for the DataThread conference by Voltron Data.
+
+[![Title slide screenshot](index-img.png)](https://jthomasmock.github.io/arrow-dplyr/)
---
+++ generate-meta.R
@@ -0,0 +1,10 @@
+metathis::meta() |> 
+  metathis::meta_social(
+    title = "Outrageously efficient exploratory data analysis with Apache Arrow and dplyr",
+    description = "A 10 minute lightning talk on all things arrow + dplyr",
+    url = "https://jthomasmock.github.io/arrow-dplyr/#/",
+    image = "https://raw.githubusercontent.com/jthomasmock/arrow-dplyr/master/index-img.png",
+    image_alt = "The title slide of the presentation, with the arrow and dplyr hex logos",
+    twitter_card_type = "summary_large_image",
+    twitter_creator = "@thomas_mock"
+  )
